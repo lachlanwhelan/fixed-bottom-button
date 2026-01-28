8d70b793bc3634c234e89e4fe843b01ee52c057b
Lachlan Whelan <lachlan.whelan@accenture.com>
2026-01-28 16:24:24 +1100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Box, Button, Input } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Box p="4" height="200vh">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <Box position="fixed" bottom={0} right={0} left={0} top={0} backgroundColor="white" pointerEvents="none">
+          <Input type='search' fontSize="16px" placeholder="Chakra UI Input" mb="4" />
+        <Button position="absolute" left={0} right={0} bottom="env(safe-area-inset-bottom)" mb="4" colorScheme="teal" size="lg">
+          Click Me
+        </Button>
+      </Box>
+    </Box>
   )
 }
 
